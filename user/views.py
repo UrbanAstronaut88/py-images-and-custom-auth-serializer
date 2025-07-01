445a11e02c8d3a1e1a927145aab89a20c8bfacd1
BondarenkoKirill <bku089@gmail.com>
2025-07-01 12:19:54 +0300

    hotfix

--- user/views.py
+++ user/views.py
@@ -5,7 +5,6 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
 from rest_framework.settings import api_settings
 from rest_framework.authtoken.models import Token
-
 from user.serializers import UserSerializer, CustomTokenSerializer
 
 
